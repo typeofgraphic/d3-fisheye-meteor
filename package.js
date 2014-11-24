159c97621b9bdeb2bff9f5784207472f4c4a43ff
PB <pbooth80@gmail.com>
2014-11-24 18:41:59 +0000

    changed details to fisheye.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-    summary: "Sankey D3 Plugin for Meteor"
+    summary: "Fisheye D3 Plugin for Meteor"
 });
 
 Package.on_use(function(api, where) {
     api.use(['d3'], 'client');
-    api.add_files(['sankey.js']);
+    api.add_files(['fisheye.js']);
 
     if (api.export) {
-        api.export('sankey');
+        api.export('fisheye');
     }
 });
